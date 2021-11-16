ee4701216e9a854c64067d1a1931e8a737a3cc4f
Fady Taher <fady.taher@izea.com>
2021-11-16 23:24:39 +0200

    using tag

--- build/core/TikTok.js
+++ build/core/TikTok.js
@@ -231,7 +231,7 @@ class TikTokScraper extends events_1.EventEmitter {
         if (this.scrapeType !== 'trend' && !this.input) {
             return this.returnInitError('Missing input');
         }
-        console.log('version marker 3');
+        console.log('version marker 4 | v2.0.0');
         await this.mainLoop();
         if (this.event) {
             return this.emit('done', 'completed');
